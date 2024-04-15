6f3425cf899279dcca92f88f4dc34e9b6401f269
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2024-04-15 15:53:21 +0300

    change

--- ekzamen17/Form1.cs
+++ ekzamen17/Form1.cs
@@ -19,7 +19,7 @@ namespace ekzamen17
             InitializeComponent();
             
         }
-        public string path = "Host=localhost;Username=postgres;Password=cxNTVJas;Database=";
+        public string path;
 
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,29 +30,52 @@ namespace ekzamen17
         // Нажатие на кнопку  "Создать заявление"
         private void add_letter_Click(object sender, EventArgs e)
         {
-            NpgsqlConnection connect = new NpgsqlConnection("Host=localhost;Username=postgres;Password=cxNTVJas;Database=17ekzamen");
-            string query = "insert into data_ab(" +
-                "id, surname, name, patronymic,level_edu,coins_edu,snils,seria,number_pass,when_pass," +
-                "how_pass,phone,email,path_edu,address_pass,institut) values " +
-                "(@id, @surname, @name, @patronymic,@level_edu,@coins_edu,@snils,@seria,@number_pass," +
-                "@when_pass,@how_pass,@phone,@email,@path_edu,@address_pass,@institut)" ;
-            NpgsqlCommand cmd = new NpgsqlCommand(query, connect);
-            cmd.Parameters.AddWithValue("@id", textBox18.Text);
-            cmd.Parameters.AddWithValue("@surname", textBox19.Text);
-            cmd.Parameters.AddWithValue("@name", textBox20.Text);
-            cmd.Parameters.AddWithValue("@patronymic", textBox21.Text);
-            cmd.Parameters.AddWithValue("@level_edu", comboBox7.Text);
-            cmd.Parameters.AddWithValue("@coins_edu", textBox22.Text);
-            cmd.Parameters.AddWithValue("@snils", textBox23.Text);
-            cmd.Parameters.AddWithValue("@seria", textBox33.Text);
-            cmd.Parameters.AddWithValue("@number_pass", textBox24.Text);
-            cmd.Parameters.AddWithValue("@when_pass", textBox25.Text);
-            cmd.Parameters.AddWithValue("@how_pass", textBox26.Text);
-            cmd.Parameters.AddWithValue("@phone", textBox27.Text);
-            cmd.Parameters.AddWithValue("@email", textBox28.Text);
-            cmd.Parameters.AddWithValue("@path_edu", textBox9.Text);
-            cmd.Parameters.AddWithValue("@address_pass", textBox29.Text);
-            cmd.Parameters.AddWithValue("@institut", textBox30.Text);
+            try
+            {
+                NpgsqlConnection connect = new NpgsqlConnection("Host=localhost;Username=postgres;Password=cxNTVJas;Database=17ekzamen");
+                string query = "insert into data_ab(" +
+                    "id, surname, name, patronymic,level_edu,coins_edu,snils,seria,number_pass,when_pass," +
+                    "how_pass,phone,email,path_edu,address_pass,institut) values " +
+                    "(@id, @surname, @name, @patronymic,@level_edu,@coins_edu,@snils,@seria,@number_pass," +
+                    "@when_pass,@how_pass,@phone,@email,@path_edu,@address_pass,@institut)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, connect);
+                cmd.Parameters.AddWithValue("@id", textBox18.Text);
+                cmd.Parameters.AddWithValue("@surname", textBox19.Text);
+                cmd.Parameters.AddWithValue("@name", textBox20.Text);
+                cmd.Parameters.AddWithValue("@patronymic", textBox21.Text);
+                cmd.Parameters.AddWithValue("@level_edu", comboBox7.Text);
+                cmd.Parameters.AddWithValue("@coins_edu", textBox22.Text);
+                cmd.Parameters.AddWithValue("@snils", textBox23.Text);
+                cmd.Parameters.AddWithValue("@seria", textBox33.Text);
+                cmd.Parameters.AddWithValue("@number_pass", textBox24.Text);
+                cmd.Parameters.AddWithValue("@when_pass", textBox25.Text);
+                cmd.Parameters.AddWithValue("@how_pass", textBox26.Text);
+                cmd.Parameters.AddWithValue("@phone", textBox27.Text);
+                cmd.Parameters.AddWithValue("@email", textBox28.Text);
+                cmd.Parameters.AddWithValue("@path_edu", path);
+                cmd.Parameters.AddWithValue("@address_pass", textBox29.Text);
+                cmd.Parameters.AddWithValue("@institut", textBox30.Text);
+                connect.Open();
+                cmd.ExecuteNonQuery();
+                connect.Close();
+
+                NpgsqlConnection con = new NpgsqlConnection("Host=localhost;Username=postgres;Password=cxNTVJas;Database=17ekzamen");
+                string sql = "insert into login_password(" +
+                    "id, login, password) values " +
+                    "(@id, @login, @password)";
+                NpgsqlCommand command = new NpgsqlCommand(sql, con);
+                command.Parameters.AddWithValue("@id", textBox18.Text);
+                command.Parameters.AddWithValue("@path_edu", textBox31.Text);
+                command.Parameters.AddWithValue("@address_pass", textBox32.Text);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -61,14 +84,15 @@ namespace ekzamen17
                 comboBox8.Text, comboBox9.Text, comboBox10.Text, comboBox11.Text, comboBox12.Text);
             print.ShowDialog();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            path = file.FileName;
+        }
     }
 
 
-        // Загрузка документа
-        private void documentBut_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog file = new OpenFileDialog();
-            string path = file.FileName;
-        }
+        
     }
 
